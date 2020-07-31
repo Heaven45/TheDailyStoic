bbbeefe7915674330a4035a0f4e8bdbe3f081255
fedulov-s <fedulov-s@yamoney.ru>
2020-07-31 22:51:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/thedailystoic/PageActivity.java
@@ -0,0 +1,15 @@
+package com.example.thedailystoic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class PageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+    }
+}
