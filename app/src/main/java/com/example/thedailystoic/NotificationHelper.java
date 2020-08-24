116ed45c596bf3b0cdc89185db2683cd9c20dcb2
fedulov-s <fedulov-s@yamoney.ru>
2020-08-24 22:51:39 +0300

    добавлено хранение времени таймера в sharedPreferences

--- app/src/main/java/com/example/thedailystoic/NotificationHelper.java
+++ app/src/main/java/com/example/thedailystoic/NotificationHelper.java
@@ -5,13 +5,17 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.ContextWrapper;
+
 import android.os.Build;
 import androidx.core.app.NotificationCompat;
-
+import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationHelper extends ContextWrapper {
+
     public static final String channelID = "channelID";
     public static final String channelName = "Channel Name";
+
+    private NotificationManagerCompat notificationManager;
 
     private NotificationManager mManager;
 
--- app/src/main/java/com/example/thedailystoic/SettingsActivity.java
+++ app/src/main/java/com/example/thedailystoic/SettingsActivity.java
@@ -5,11 +5,13 @@ import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
@@ -24,6 +26,11 @@ public class SettingsActivity extends AppCompatActivity implements TimePickerDia
 
     TextView textViewSetTime;
     TextView textViewTime;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,9 @@ public class SettingsActivity extends AppCompatActivity implements TimePickerDia
                 timePicker.show(getSupportFragmentManager(), "time picker");
             }
         });
+
+        loadData();
+        updateViews();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -57,7 +67,10 @@ public class SettingsActivity extends AppCompatActivity implements TimePickerDia
         String timeText = "Alarm set for: ";
         timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
         textViewTime.setText(timeText);
+
+        saveData();
     }
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void startAlarm(Calendar c) {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
@@ -67,5 +80,21 @@ public class SettingsActivity extends AppCompatActivity implements TimePickerDia
             c.add(Calendar.DATE, 1);
         }
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT, textViewTime.getText().toString());
+        editor.apply();
+        Toast.makeText(this, "Time saved", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+    }
+    public void updateViews() {
+        textViewTime.setText(text);
     }
 }
